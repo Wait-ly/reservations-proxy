52ae2467193b0cdfcb16b4837e663a8a3815d037
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-09-21 11:23:27 -0700

    ready for deployment

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const port = 3000;
 
 
-app.use('/:id/', express.static('public'));
+app.use('/reservations/:id/', express.static('public'));
 
 
 app.listen(port, () => { console.log(`Proxy server listening on port ${port}`); });
